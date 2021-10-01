536a3424568920670c58be02703d25061b03fa33
Scott Griffin <mscottgriffin@gmail.com>
2021-10-01 09:50:37 -0400

    rewrote get request using axios client baseURL

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -2,32 +2,39 @@ import React, { memo, useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import axios from 'axios';
 
-const Main = memo(() => {
+//
+// ─── SUPERHERO API https://akabab.github.io/superhero-api/api/
+//
+const client = axios.create({
+  baseURL: 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/',
+});
+// API ENDPOINTS
+const all_endpoint = 'all.json';
+const id_endpoint = 'id/3.json';
+
+const Main = () => {
   //
   // ─── STATE HOOKS ────────────────────────────────────────────────────────────────
   //
-  const [displayHero, setDisplayHero] = useState('[]');
-  const fetchHero = async () => {
-    try {
-      const response = await axios(
-        'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/30.json'
-      );
-      setDisplayHero(response.data);
-    } catch (err) {
-      console.error(err);
-    }}
+  const [heroes, setHeroes] = useState([]);
+  //
+  // ─── API 'GET' REQUEST ───────────────────────────────────────────────────────────────────
+  //
+  useEffect(() => {
+    async function getHeroes() {
+      const response = await client.get(id_endpoint);
+      setHeroes(response.data);
+    }
+    getHeroes();
+  }, []);
 
-    useEffect(() => {
-      fetchHero();
-    }, []);
 
-    return (
-      <div>
-        <button onClick={fetchHero}>See Hero</button>
-        <p>{displayHero.name}</p>
-      </div>
-    );
-  }
-);
+  return (
+    <div>
+      <h1>{heroes.name}</h1>
+      <p>{heroes.id}</p>
+    </div>
+  );
+};
 
 export default Main;
